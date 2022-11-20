5756d93dc51fd77bfd33dbfc275728673dd732fb
Ekawardana <082312931650as@gmail.com>
2022-11-20 15:31:52 +0700

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import {getMovieList, searchMovie} from "./api" //Import dari api.js
+import { useEffect, useState } from 'react'; //Import react
 
-function App() {
+const App = () => {
+const [popularMovies, setPopularMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setPopularMovies(result)
+    })
+  }, [])
+
+  // data gambar dari API di .env
+  const urlImage = process.env.REACT_APP_BASEIMGURL
+
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) =>{
+      return(
+        // List Film
+          <div className="Movie-wrapper" key={i}>
+            <div className="Movie-title">{movie.title}</div>
+            <img className="Movie-image" src={`${urlImage}/${movie.poster_path}`}/>
+            <div className="Movie-date">Release : {movie.release_date}</div>
+            <div className="Movie-rate">{movie.vote_average}</div>
+          </div> 
+      )
+    })
+  }
+
+  const search = async(q) =>{
+    if(q.length > 3 ){
+      const query = await searchMovie(q)
+      setPopularMovies(query.results)
+    }
+  }
+
+  console.log({popularMovies: popularMovies})
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Eka Movie FILMS</h1>
+          <input placeholder='Cari Film Kesukaan...' 
+            className='Movie-search'
+            onChange={({target}) => search(target.value)}
+          />
+        <div className="Movie-container">
+              <PopularMovieList/>
+        </div>
       </header>
     </div>
   );
